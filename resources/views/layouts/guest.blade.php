2cb4bf2e350ce6faa730c8481e2abf8566914b2a
Queue-it Kevin <kevinlhall72@gmail.com>
2022-06-08 16:14:20 +0200

    Nav to landing

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -28,7 +28,10 @@
                     <a href="{{ url('/dashboard') }}"
                        class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
                 @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                    <a href="/" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+
+                    <a href="{{ route('login') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Log
+                        in</a>
 
                     @if (Route::has('register'))
                         <a href="{{ route('register') }}"
